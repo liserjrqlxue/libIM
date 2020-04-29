917d6baea7388adb10a7e11c107d798140d1f4ca
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2020-04-29 15:05:38 +0800

    set libIM.Throttle public

--- var.go
+++ var.go
@@ -4,4 +4,4 @@ var LaneInput = "null"
 var Pipeline = "."
 var Trio = []string{"proband", "father", "mother"}
 var threshold = 12
-var throttle = make(chan bool, threshold)
+var Throttle = make(chan bool, threshold)
